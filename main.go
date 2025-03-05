8ecf26a40992007b120c83a5fa3ca737caeeb866
Jelle Jacobs <jelle.jacobs@gmail.com>
2025-03-05 17:01:21 +0100

    receive and update files on the bucket with http requests

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"strings"
 
+	"github.com/gin-gonic/gin"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
@@ -18,17 +20,17 @@ const (
     bucket = "test-bucket-jelle"
 )
 
-func main() {
+var minioClient *minio.Client
 
-    // initializing a new minio client
-    minioClient, err := minio.New(endpoint, &minio.Options{
+func initMinio() {
+    var err error
+    minioClient, err = minio.New(endpoint, &minio.Options{
         Creds: credentials.NewStaticV4(accessKey, secretKey, ""),
         Secure: false,
     })
     if err != nil {
         log.Fatalln(err)
     }
-    
     // Checking if the fucket exists
     ctx := context.Background();
     exists, err := minioClient.BucketExists(ctx, bucket)
@@ -43,32 +45,66 @@ func main() {
         }
         fmt.Println("Bucket created succesfully")
     }
+}
 
-    // Upload a text file to this bucket
-    objectName := "example.txt"
-    textContext := "Hello, the file should be updated"
-    err = uploadText(minioClient, objectName, textContext)
+func uploadFile(c *gin.Context) {
+    file, err := c.FormFile("file")
     if err != nil {
         log.Fatalf("error: %s\n", err.Error())
+        c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
     }
 
-    // Retrieve the text file
-    retrievedText, err := getText(minioClient, objectName)
+    src, err := file.Open()
     if err != nil {
         log.Fatalf("error: %s\n", err.Error())
+        c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
     }
 
-    fmt.Println("Retrieved text: ", retrievedText)
-
-    objectName2 := "testTekst.txt"
-    // Retrieve the text file
-    retrievedText1, err := getText(minioClient, objectName2)
+    defer src.Close()
+    objectName := file.Filename
+    ctx := context.Background()
+    _, err = minioClient.PutObject(ctx, bucket, objectName, src, file.Size, minio.PutObjectOptions{
+            ContentType: "application/octet-stream"})
     if err != nil {
         log.Fatalf("error: %s\n", err.Error())
+        c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
     }
-    fmt.Println("Retrieved text: ", retrievedText1)
+
+    c.JSON(http.StatusOK, gin.H{"message" : "File uploaded succesfully"})
+}
+
+func getFile(c *gin.Context) {
+    objectName := c.Param("filename")
+    ctx := context.Background()
+    object, err := minioClient.GetObject(ctx, bucket, objectName, minio.GetObjectOptions{})
+    if err != nil {
+        log.Fatalf("error: %s\n", err.Error())
+        c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
+    }
+    defer object.Close()
+
+    content, err := io.ReadAll(object)
+    if err != nil {
+        log.Fatalf("error: %s\n", err.Error())
+        c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
+    }
+
+    c.String(http.StatusOK, string(content))
+
+}
 
 
+func main() {
+    initMinio()
+
+    r := gin.Default()
+
+    r.POST("/upload", uploadFile)
+    r.GET("/files/:filename", getFile)
+
+    if err := r.Run(":8080"); err != nil {
+        log.Fatalf("error: %s\n", err.Error())
+    }
 }
 
 func uploadText(client *minio.Client, objectName, content string) error {
